a31795a1898168fe2ef68f07ce6c3291a3b7d76b
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2020-03-28 15:46:58 +0530

    Updates

---
+++ precache-manifest.04301ca62adbd79396f26f113a3752bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646bdcaae72be8d82d42036344299b24",
+    "url": "/Joplin-proposal-draft/index.html"
+  },
+  {
+    "revision": "9371fe5726f5d906d103",
+    "url": "/Joplin-proposal-draft/static/css/main.fc782ab4.chunk.css"
+  },
+  {
+    "revision": "0c00ee7208dd398f13f8",
+    "url": "/Joplin-proposal-draft/static/js/2.017af1ed.chunk.js"
+  },
+  {
+    "revision": "d98af3128269add930c8c721d083bbed",
+    "url": "/Joplin-proposal-draft/static/js/2.017af1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9371fe5726f5d906d103",
+    "url": "/Joplin-proposal-draft/static/js/main.d3ee7b8b.chunk.js"
+  },
+  {
+    "revision": "3a346fa237cf8d94ef29",
+    "url": "/Joplin-proposal-draft/static/js/runtime-main.0d43f354.js"
+  }
+]);
